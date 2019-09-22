bddb3d4ddc17aee3dc9b8a1f6c2649c22a209969
Lu Zhao <luzhao0229@gmail.com>
2019-09-22 13:56:09 +1200

    the sixth

--- src/sixth.java
+++ src/sixth.java
@@ -6,30 +6,21 @@ public class sixth {
         System.out.println(str.equals("LDREOEIIECIHNTSG"));
     }
 
-    public static String convert(String s, int numRows) {
-        if(s==null || s.length() < 1) return "";
-        if(s.length() <= 2 || numRows==1) return s;
+    private static String convert(String s, int numRows) {
+        if(numRows==1) return s;
         int gap = numRows * 2 - 2;
-        int len = s.length() / gap + 1;
-        char[][] cArray = new char[len][gap];
-        for(int i = 0;i < s.length();i++) {
-            int first = i / gap;
-            int second = i % gap;
-            cArray[first][second] = s.charAt(i);
-        }
-        StringBuffer sb = new StringBuffer();
-        for(int j=0;j<numRows;j++) {
-            for(int i=0;i<len;i++) {
-                if(cArray[i][j]!='\0') {
-                    sb.append(cArray[i][j]);
-                }
-                if(j!=0 && j != numRows-1) {
-                    if(cArray[i][gap - j]!='\0') {
-                        sb.append(cArray[i][gap - j]);
-                    }
+        int len = s.length();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<numRows;i++) {
+            for(int j=0;j+i < len;j+= gap) {
+                sb.append(s.charAt(i+j));
+                if(i!=0 && i!=numRows-1 && j+gap-i < len) {
+                    sb.append(s.charAt(j+gap-i));
                 }
             }
         }
         return sb.toString();
+
     }
 }
