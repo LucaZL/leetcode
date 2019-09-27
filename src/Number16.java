c5305322da6f6c98d12299a2adb3210ca6d75f8a
Lu Zhao <luzhao0229@gmail.com>
2019-09-27 22:51:36 +1200

    16

--- src/Number16.java
+++ src/Number16.java
@@ -9,26 +9,36 @@ public class Number16 {
     }
 
     private static int threeSumClosest(int[] nums, int target) {
-        int result = nums[0] + nums[1] + nums[2];
+        int result = Integer.MAX_VALUE;
         int len = nums.length;
         Arrays.sort(nums);
-        int L, R;
-        for(int i=0;i<len;i++) {
+        int L, R, diff = Integer.MAX_VALUE;
+        for(int i=0;i<len-2;i++) {
             if(i>0 && nums[i] == nums[i-1]) continue;
+            int areaLargest = nums[i] + nums[len-2] + nums[len - 1];
+            int areaSmallest = nums[i] + nums[i+1] + nums[i+2];
+            if(areaLargest < target) {
+                if(diff > Math.abs(areaLargest - target)) {
+                    result = areaLargest;
+                    diff = Math.abs(areaLargest - target);
+                }
+                continue;
+            }else if(areaSmallest > target) {
+                if(diff > Math.abs(areaSmallest - target)) {
+                    result = areaSmallest;
+                    diff = Math.abs(areaSmallest - target);
+                }
+                continue;
+            }
             L = i+1; R = len - 1;
             while(L < R) {
                 int sum = nums[i] + nums[L] + nums[R];
-                if(Math.abs(result - target) > Math.abs(sum - target)) {
+                if(diff > Math.abs(sum - target)) {
                     result = nums[i] + nums[L] + nums[R];
-
+                    diff = Math.abs(sum - target);
                 }
                 if(sum == target) {
                     return sum;
-//                    result.add(Arrays.asList(nums[i], nums[L], nums[R]));
-//                    while(L < R && nums[L]==nums[L+1]) L++;
-//                    while(L < R && nums[R]==nums[R-1]) R--;
-//                    L++;
-//                    R--;
                 }else if(sum < target) {
                     L++;
                 }else if(sum > target) {
