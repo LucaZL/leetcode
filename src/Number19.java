b50ab119d303ffe14da0f209123a0deb09d00329
Lu Zhao <luzhao0229@gmail.com>
2019-09-30 20:50:10 +1300

    19

--- src/Number19.java
+++ src/Number19.java
@@ -12,7 +12,7 @@ public class Number19 {
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        ListNode result = removeNthFromEnd(node1, 5);
+        ListNode result = removeNthFromEnd(node5, 1);
         while(result!=null) {
             System.out.println("result="+result.val);
             result = result.next;
@@ -20,23 +20,38 @@ public class Number19 {
     }
 
     private static ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next==null) return null;
-        List<ListNode> list = new ArrayList();
-        list.add(head);
-        ListNode nextNode = head.next;
-        while(nextNode!=null) {
-            list.add(nextNode);
-            nextNode = nextNode.next;
+        ListNode tempHead = new ListNode(0);
+        tempHead.next = head;
+        ListNode first = tempHead;
+        ListNode second = tempHead;
+        // the first need to move n+1
+        for(int i=0;i<=n;i++) {
+            first = first.next;
         }
-        int len = list.size();
-        if(len == n) {
-            return list.get(1);
-        }else if(n == 1) {
-            list.get(len - 2).next = null;
-        }else{
-            list.get(len - n - 1).next = list.get(len - n + 1);
+        while(first!=null) {
+            first = first.next;
+            second = second.next;
         }
-        return head;
+        second.next=second.next.next;
+        return tempHead.next;
+
+//        if(head.next==null) return null;
+//        List<ListNode> list = new ArrayList();
+//        list.add(head);
+//        ListNode nextNode = head.next;
+//        while(nextNode!=null) {
+//            list.add(nextNode);
+//            nextNode = nextNode.next;
+//        }
+//        int len = list.size();
+//        if(len == n) {
+//            return list.get(1);
+//        }else if(n == 1) {
+//            list.get(len - 2).next = null;
+//        }else{
+//            list.get(len - n - 1).next = list.get(len - n + 1);
+//        }
+//        return head;
     }
 
 }
