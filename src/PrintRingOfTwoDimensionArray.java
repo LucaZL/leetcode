82a10ffec5082cab0e237b58a56f31f2c0a07768
stupidluca <luzhao0229@gmail.com>
2019-11-30 11:53:30 +0800

    print a two dimentional array like a ring

---
+++ src/PrintRingOfTwoDimensionArray.java
@@ -0,0 +1,38 @@
+public class PrintRingOfTwoDimensionArray {
+
+    public static void main(String[] args) {
+        printNumbers(7);
+    }
+
+    public static void printNumbers(int n) {
+        int[][] inputs = new int[n][n];
+        int count = 0;
+        System.out.println("print origin numbers start-----");
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                inputs[i][j] = ++count;
+                System.out.printf("%4s ",count);
+            }
+            System.out.println();
+        }
+        System.out.println("print origin numbers end-----");
+
+        int[] direction = {1, n, -1, -n};
+        int dir_change = 0;
+        int current = -1;
+        int currentLineNums = n;
+        while(true) {
+            for(int j=0;j<currentLineNums;j++) {
+                current += direction[dir_change % 4];
+                int row = current / n;
+                int col = current % n;
+                System.out.printf("%4s", inputs[row][col]);
+            }
+            System.out.println();
+            if(dir_change % 2 == 0) currentLineNums--;
+            if(currentLineNums <=0) break;
+            dir_change++;
+        }
+        System.out.println("print changed numbers end----");
+    }
+}
